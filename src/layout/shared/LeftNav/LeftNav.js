55d0add0614888c2d0888f33198dd4c4521bb365
Abu Raihan <raihanahmed01973@gmail.com>
2023-01-26 23:10:54 +0600

    Showing category done

--- src/layout/shared/LeftNav/LeftNav.js
+++ src/layout/shared/LeftNav/LeftNav.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
-    return (
-        <div>
-            <h2>left nav</h2>
-        </div>
-    );
+  const [categories, setCategoris] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/news-categories")
+      .then((res) => res.json())
+      .then((data) => setCategoris(data));
+  }, []);
+  return (
+    <div>
+      <h4>All categories {categories.length}</h4>
+      {categories.map((category) => (
+        <p key={category.id}>
+          {" "}
+          <Link to={`/category/${category.id}`}>{category.name}</Link>
+        </p>
+      ))}
+    </div>
+  );
 };
 
 export default LeftNav;
